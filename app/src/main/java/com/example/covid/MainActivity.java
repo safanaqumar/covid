662e88d514366dda9641a6eec4aab074fd3415ac
syedamariarasheed <syedamariarasheed@gmail.com>
2020-11-16 21:13:31 +0500

    fixed minor issues, change color Accent.

--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -11,9 +11,11 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.PendingIntent;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
@@ -93,30 +95,41 @@ public class MainActivity extends AppCompatActivity {
          logout.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 SessionManagement sessionManagement = new SessionManagement(MainActivity.this);
-                 sessionManagement.removeSession();
-                 Toast.makeText(getApplicationContext(),"logout", Toast.LENGTH_LONG ).show();
-                 String userID = sessionManagement.getSession();
-                 if (userID =="default")
-                 {
-                     Toast.makeText(getApplicationContext(),"logout", Toast.LENGTH_LONG ).show();
-                     Intent intent =new Intent();
-                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
-                     logout.setVisibility(View.INVISIBLE);
-                     login.setVisibility(View.VISIBLE);
-                     donation.setVisibility(View.INVISIBLE);
+                 AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+                 builder1.setMessage("Are you sure you want to Logout?");
+                 builder1.setCancelable(true);
+                 builder1.setPositiveButton(
+                         "Yes",
+                         new DialogInterface.OnClickListener() {
+                             public void onClick(DialogInterface dialog, int id) {
+                                 SessionManagement sessionManagement = new SessionManagement(MainActivity.this);
+                                 sessionManagement.removeSession();
+                                 String userID = sessionManagement.getSession();
+                                 if (userID =="default")
+                                 {
+                                     Intent intent =new Intent();
+                                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+                                     logout.setVisibility(View.INVISIBLE);
+                                     login.setVisibility(View.VISIBLE);
+                                     donation.setVisibility(View.INVISIBLE);
 
 
 
-                 }
-                 else
-                 {
+                                 }else {}
 
-                 }
+                             }
+                         });
 
+                 builder1.setNegativeButton(
+                         "No",
+                         new DialogInterface.OnClickListener() {
+                             public void onClick(DialogInterface dialog, int id) {
+                                 dialog.cancel();
+                             }
+                         });
 
-
-
+                 AlertDialog alert11 = builder1.create();
+                 alert11.show();
 
 
              }
@@ -211,7 +224,6 @@ public class MainActivity extends AppCompatActivity {
         {
 
             sessionManagement.removeSession();
-            Toast.makeText(getApplicationContext(),userID, Toast.LENGTH_LONG ).show();
             logout.setVisibility(View.INVISIBLE);
             login.setVisibility(View.VISIBLE);
         }
